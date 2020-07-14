96b5abb29192f863431d815e71bcb1656d519d15
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-14 21:49:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  elsif other_deli.length > 0
+  elsif katz_deli.length > 0
     puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
-  elsif another_deli.length > 0
+  elsif katz_deli.length > 0
     
   end
 end
