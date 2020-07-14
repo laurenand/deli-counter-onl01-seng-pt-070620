c056b1adee1acbdc30fc33939028e53fa8f3d8be
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-14 21:45:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,6 @@ def line(katz_deli)
   elsif other_deli.length > 0
     puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   elsif another_deli.length > 0
-    another_deli(index)
-    puts "The line is currently: 1. #{index} 2. #{index} 3. #{index} 4. #{index} 5. #{index} 6. #{index} 7. #{index} 8. #{index} 9. #{index} 10. #{index}"
+    
   end
 end
