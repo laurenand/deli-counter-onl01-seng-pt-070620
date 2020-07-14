594acc8c4d51f587708967b88a3a18468190fdb0
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-14 23:45:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(deli)
     puts "The line is currently empty."
   else 
     current_line = "The line is currently:"
-    deli.each_with_index do |name,index|
-    puts current_line << " #{index+1}. #{name}"
+    deli.each.with_index(1) do |name,index|
+    puts current_line << " #{index}. #{name}"
   end
 end
 end
