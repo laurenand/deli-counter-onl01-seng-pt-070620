303f351dfe4a50fbdf0d76d4cd1b14c12445d049
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-14 23:07:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   else 
     current_line = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      current_line << " #{index}. #{name}"
+      current_line << " #{index+1}. #{name}"
   end
 end
 end
